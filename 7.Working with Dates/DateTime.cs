12ac4e7273422226e211dbba38161372027638a2
siba18k <dubesibahle4@gmail.com>
2024-07-27 13:55:47 +0200

    Working with Files

---
+++ 7.Working with Dates/DateTime.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Date_Time//Are an immutable type, you need to use .Add()  to modify them
+{
+    public static void datentime()
+    // static void Main(string[] args)
+    {
+        var dateTime = new DateTime(2024,7,12);
+        var now = DateTime.Now;
+        var today = DateTime.Today;
+        Console.WriteLine(dateTime);
+        Console.WriteLine(today);
+        
+        
+        Console.WriteLine($"Hour: {now.Hour}");
+        Console.WriteLine($"Minute: {now.Minute}");
+        Console.WriteLine($"Second: {now.Second}");
+
+        
+        var tomorrow = now.AddDays(800);//If you want to go to the past then use a negative number
+        Console.WriteLine(tomorrow);
+        
+        //Converting Dates to String
+        Console.WriteLine(now.ToLongDateString());
+        Console.WriteLine(now.ToShortDateString());
+        Console.WriteLine(now.ToLongTimeString());
+        Console.WriteLine(now.ToShortTimeString());
+        Console.WriteLine(now.ToString());
+        Console.WriteLine(now.ToString("yy-MMM-dd ddd hh:mm"));//When you wanna format the date
+     //Go search c#datetime format specifier if you want more   
+    }
+}
---
+++ 7.Working with Dates/Time Span.cs
@@ -0,0 +1,41 @@
+﻿namespace _7.Working_with_Dates;
+
+public class Time_Span//Represents a length of time usually Hours - Minutes
+{
+    // pubic static void tmespan()
+    static void Main(string[] args)
+    {
+        
+        //Creating
+        var timeSpan = new TimeSpan(1,2,3);
+        Console.WriteLine(timeSpan);
+
+        var timeSpan1 = new TimeSpan(1, 0, 0);
+        Console.WriteLine(timeSpan1);
+        
+        var timeSpan2 = TimeSpan.FromHours(1);
+        Console.WriteLine(timeSpan2);
+
+        var start = DateTime.Now;
+        var end = DateTime.Now.AddMinutes(2);
+        var duration = end - start;//This gives you a TimeSpan
+        Console.WriteLine($"Duration: {duration}");
+
+        //Properties
+        Console.WriteLine($"Minutes: {timeSpan.Minutes}");
+        Console.WriteLine($"Total Minutes: {timeSpan.TotalMinutes}");
+        
+        //Add and Subtract
+        Console.WriteLine($"Add Example:{timeSpan.Add(TimeSpan.FromMinutes(8))}");
+        Console.WriteLine($"Subtract Example:{timeSpan.Subtract(TimeSpan.FromMinutes(2))}");
+        
+        
+        //ToString
+        Console.WriteLine($"ToString: {timeSpan.ToString()}");
+        
+        //Parse
+        // var parsedtimeSpan = TimeSpan.Parse("01:09:12");
+        // Console.WriteLine($"Parsed: {parsedtimeSpan}");
+        Console.WriteLine($"Parse: {TimeSpan.Parse("01:04:05")}");
+    }
+}
