c00bdcb6d486519a7175cc5d4a848e08956e25ae
siba18k <dubesibahle4@gmail.com>
2024-07-30 15:14:28 +0200

    la comitta

--- 6. Lists and Arrays/obj/Debug/net8.0/6. Lists and Arrays.AssemblyInfo.cs
+++ 6. Lists and Arrays/obj/Debug/net8.0/6. Lists and Arrays.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("6. Lists and Arrays")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ac4e7273422226e211dbba38161372027638a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("6. Lists and Arrays")]
 [assembly: System.Reflection.AssemblyTitleAttribute("6. Lists and Arrays")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 7.Working with Dates/obj/Debug/net8.0/7.Working with Dates.AssemblyInfo.cs
+++ 7.Working with Dates/obj/Debug/net8.0/7.Working with Dates.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("7.Working with Dates")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12ac4e7273422226e211dbba38161372027638a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("7.Working with Dates")]
 [assembly: System.Reflection.AssemblyTitleAttribute("7.Working with Dates")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
